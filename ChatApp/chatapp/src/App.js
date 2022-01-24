b683b13dd470d840eebc12c145f76a52875ba49b
Zainab Zahra <szz@uw.edu>
2022-01-23 22:37:59 -0800

    Chat App

---
+++ ChatApp/chatapp/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <div className="logo" />
+        <span className="title">chatter</span>
+      </header>
+      <footer className="footer">
+        <input className="text-input" />
+        <button className="send">↑</button>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
