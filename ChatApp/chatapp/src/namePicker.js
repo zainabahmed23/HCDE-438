b0f9fa84af986d28cfe8fa65a787db6b2be510ef
Zainab Zahra <szz@uw.edu>
2022-02-13 23:49:08 -0800

    Assignment 6

--- ChatApp/chatapp/src/namePicker.js
+++ ChatApp/chatapp/src/namePicker.js
@@ -1,13 +1,38 @@
-import {useState} from React
+import { useState } from "react";
+import { FiEdit } from "react-icons/fi";
 
-function NamePicker() {
-    // manages the editing state 
-    const [editName, setEditName] = useState(false)
-    // manages the name state
-    const [name, setName] = useState('')
+function NamePicker(props) {
+ // manages the name state
+  const [name, setName] = useState("");
+// toggles between showing and hiding the input
+  const [showInput, setShowInput] = useState(false);
+
+  // you clicked ok!
+  function ok() {
+    props.setUsername(name);
+    setShowInput(false);
+  }
+
+  if (showInput) {
+    return (
+      <div className="name-picker">
+        <input
+          className="name-picker-input"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <button className="name-picker-button" onClick={ok}>
+          OK
+        </button>
+      </div>
+    );
+  }
+  return (
+    <div className="name-picker">
+      <span className="name-picker-name">{name || "Set Username:"}</span>
+      <FiEdit size="24" onClick={() => setShowInput(true)} />
+    </div>
+  );
 }
 
-return (
-  );
-
-export default NamePicker
+export default NamePicker;
